d680bc2f53ab473ce3a9169d76f850911a0b62f6
ymghazli <ymghazli@c4r6p4.1337.ma>
2023-12-13 19:24:14 +0100

    Yassine

---
+++ ft_strlcat.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ymghazli <ymghazli@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/27 12:44:07 by yassine           #+#    #+#             */
+/*   Updated: 2023/12/10 18:12:35 by ymghazli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdio.h>
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	size_t	i;
+
+	dest_len = 0;
+	src_len = 0;
+	i = 0;
+	while (dest[i] != '\0' && i < size)
+	{
+		dest_len++;
+		i++;
+	}
+	i = 0;
+	src_len = ft_strlen(src);
+	i = 0;
+	while (i < src_len && dest_len + i + 1 < size)
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	if (dest_len < size)
+	{
+		dest[dest_len + i] = '\0';
+	}
+	return (dest_len + src_len);
+}
+
+
+// #include<string.h>
+// // #include <.h>
+// int main(){
+// 	char *p = NULL;
+// 	char *o = NULL;
+
+// 	printf("%s\n", strlcat(p, o, 5));
+// 	// printf("%s", strchr(p, 'i'));
+// }
